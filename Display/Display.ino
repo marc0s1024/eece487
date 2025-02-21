f0ca3b01ee9a161c9e73431098595b54818afb2f
cotty52 <cjo1102.52@gmail.com>
2025-02-21 18:59:56 -0500

    Created main working directory
    Should probably rename folders/files but this is where the main program will be

---
+++ Display/Display.ino
@@ -0,0 +1,16 @@
+// Main File
+
+#include UART_Decode.cpp
+#include Display_Functions.cpp
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
